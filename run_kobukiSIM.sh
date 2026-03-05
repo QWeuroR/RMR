3d431fdc5b3c55705952ed42ef6505d35584e70c
QWeuroR <qwerjakjan@gmail.com>
2026-03-05 08:16:16 +0100

    added prednasky

---
+++ run_kobukiSIM.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/home/qweuror/Qt/6.6.2/gcc_64/lib:$LD_LIBRARY_PATH
+/home/qweuror/FEI/ING/2_S/RMR/kobukiSIM-1.0.0-Linux.tar-ubuntu-24/kobukiSIM-1.0.0-Linux/bin/kobukiSIM
